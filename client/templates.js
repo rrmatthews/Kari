464e12a5e3abc32a24e33719e4853f1dc423eed1
R Matthews <remimatthews@brandeis.edu>
2017-06-19 00:36:14 -0400

    add new product section...prevents adding blank entries

--- client/templates.js
+++ client/templates.js
@@ -1,7 +1,23 @@
-Template.songentry.helpers({
-  songlist() {return Songs.find()},
-})
-
 Template.haircare.helpers({
   hairline() {return Hair.find()},
+  productlist() {return Products.find()}
+})
+
+
+Template.haircare.events({
+  'click button' (element, instance) {
+    const name = instance.$('#name').val();
+    const type = $("#ptype :selected").text();
+    const brand = instance.$('#brand').val();
+
+    instance.$('#name').val("");
+    instance.$('#brand').val("");
+    if (name != "" || brand != "") {
+      console.log('adding '+name);
+      Products.insert({Type:type,Brand:brand,Name:name});
+    } else {
+      console.log('Invalid Entry...field is blank?');
+    }
+
+  }
 })
--- server/main.js
+++ server/main.js
@@ -2,10 +2,10 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  Songs.remove({});  // clear the database
-  Songs.insert({Title:'Antidote',Artist:'Travis Scott'});
-  Songs.insert({Title:'The Waters',Artist:'Mick Jenkins'});
 
   Hair.remove({});  // clear the database
-  Hair.insert({Looking:'Codnditioner',Type:'4C'});
+  Hair.insert({Looking:'Conditioner',Type:'4B'});
+
+  Products.remove({});  // clear the database
+  Products.insert({Name:'Coconut Milk Anti-Breakage Serum',Type:'Product',Brand:'OGX'});
 });
